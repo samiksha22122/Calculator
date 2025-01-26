4710d6cd613db135e6fc81031b615e421ea9f494
samiksha22122 <samikshagodghate22@gmail.com>
2025-01-26 21:39:30 +0530

    added division feature

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 from addition import add
 from substraction import sub
 from multiplication import mul
+from division import div
 
 def main():
     print("Welcome to the Calculator!")
     print("1. Addition")
     print('2. Substraction')
     print('3 Multiplication')
+    print('4. Division')
 
     # Get user choice
     choice = input("Enter your choice (1 for Addition, 2 for Substraction 3 for multiplication): ")
@@ -43,8 +45,19 @@ def main():
             print(f"The result of multiplication is: {result}")
         except ValueError:
             print("Invalid input! Please enter numeric values.")
+    elif choice == '4':
+        try:
+            num1 = float(input("Enter the first number: "))
+            num2 = float(input("Enter the second number: "))
+            
+            # Perform division
+            result = div(num1, num2)
+            print(f"The result of div is: {result}")
+        except ValueError:
+            print("Invalid input! Please enter numeric values.")
     else:
         print("Invalid choice!")
+
 
 if __name__ == "__main__":
     main()
