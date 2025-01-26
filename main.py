4c461ea5ac751ee9fe82a69c4384a5183c0f2d0f
samiksha22122 <samikshagodghate22@gmail.com>
2025-01-26 20:43:35 +0530

    substraction function created

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # Importing the add function from addition.py
 from addition import add
+from substraction import sub
 
 def main():
     print("Welcome to the Calculator!")
     print("1. Addition")
+    print('2. Substraction')
 
     # Get user choice
-    choice = input("Enter your choice (1 for Addition): ")
+    choice = input("Enter your choice (1 for Addition, 2 for Substraction): ")
 
     if choice == '1':
         # Get user input for numbers
@@ -17,6 +19,16 @@ def main():
             # Perform addition
             result = add(num1, num2)
             print(f"The result of addition is: {result}")
+        except ValueError:
+            print("Invalid input! Please enter numeric values.")
+    elif choice == '2':
+        try:
+            num1 = float(input("Enter the first number: "))
+            num2 = float(input("Enter the second number: "))
+            
+            # Perform addition
+            result = sub(num1, num2)
+            print(f"The result of substraction is: {result}")
         except ValueError:
             print("Invalid input! Please enter numeric values.")
     else:
